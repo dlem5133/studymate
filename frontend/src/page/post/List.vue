7a156b9709b42ac12bf0046a8af0a14262163b5f
박재림 <dbahck200@gmail.com>
2020-08-03 12:53:57 +0900

    0803_update

--- frontend/src/page/post/List.vue
+++ frontend/src/page/post/List.vue
@@ -48,15 +48,10 @@
               <!-- -->
               <div class="input-group md-form form-sm form-2 pl-0">
                 <input class="form-control my-0 py-1 amber-border" v-model="selectCategory.optionstext" type="text" placeholder="검색어를 입력해주세요" aria-label="Search">
-                          <span class="input-group-text purple lighten-3" id="basic-text1">
-          <mdbIcon icon="search"/>
-        </span>
-                  <div class="input-group-append">
-                  
-                    <button class="input-group-text" id="addon-wrapping"  @click="categorySubmit"><img class="col-9 col-md-9"src="../../assets/img/search_icon.png"/></button>                  
-                  </div>
+                  <button class="input-group-text purple lighten-3" id="basic-text1 addon-wrapping"  @click="categorySubmit">
+                    <mdbIcon icon="search"/>
+                  </button>
               </div>
-        
             </div>
           </form>
         <!-- ======================================= 카드====================================================  -->
