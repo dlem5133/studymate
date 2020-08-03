dd764bdff5c730545d86204335a30c06235aec14
tinnia <ektha3869@naver.com>
2020-08-03 23:44:40 +0900

    0803_leader

--- frontend/src/page/post/PostCreate.vue
+++ frontend/src/page/post/PostCreate.vue
@@ -1,74 +1,85 @@
 <template>
-  <div class="container w-50">
-  <div class="card shadow my-5">
-    <div class="card-text">
-    <h1><img src="https://image.flaticon.com/icons/svg/2231/2231677.svg" class="m-5 w-25" alt=""></h1>
+  <div class="container pt-0 mt-0">
+  <div class="card border-0 w-75 mx-auto mx-5">
+    <div class="card-text mx-5">
+    <img :src="images" class="logo w-25"  alt="">
     <b-row class="m-3">
-      <b-col sm="12" class="px-1">
+      <b-col sm="12" md="8" class="px-1">
         <small class="formtitle ml-3 float-left">제목</small>
         <b-form-input v-model="postCreateDate.title" placeholder="제목" type="text"></b-form-input>
       </b-col>
-
-
-      <b-col sm="12" class="px-1">
-        <label>제목</label>
-        <b-form-input v-model="postCreateDate.title" placeholder="제목" type="text"></b-form-input>
+      <b-col sm="12" md="4" class="px-1 mb-4">
+        <div style="transform: translateY(21px);" v-if="!postCreateDate.background_image">
+          <b-form-file placeholder=" " browse-text="Image" @change="onFileChange" ></b-form-file>
+        </div>
+        <div class="d-flex" style="transform: translateY(20px);" v-else>
+          <img :src="postCreateDate.background_image" class="w-25" style="height:30px;"/>
+          <b-button class="ml-3" @click="removeImage" variant="border-0"><b-icon icon="trash"></b-icon></b-button>
+        </div>
+        <!-- <b-form-input v-model="postCreateDate.title" placeholder="제목" type="text"></b-form-input> -->
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>일정</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">일정</small>
         <b-form-input v-model="postCreateDate.bindo" placeholder="주 _회" type="text"></b-form-input>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>인원수 (3명 이상)</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle1 ml-3 float-left">인원수 (3명 이상)</small>
         <b-form-spinbutton id="demo-sb" v-model="postCreateDate.limitp" min="3" max="100"></b-form-spinbutton>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>시/도</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">시/도</small>
         <select @click="getgungu" v-model="postCreateDate.sido_code" class="custom-select">
           <option v-for="sido in allSidoCode" :key="sido.sidocode" :value="sido.sidocode">{{sido.sidoname}}</option>
         </select>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>시/군/구</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">시/군/구</small>
         <select v-model="postCreateDate.sigungu_code" class="custom-select">
           <option v-for="sido in allGugunCode" :key="sido.guguncode" :value="sido.guguncode">{{sido.gugunname}}</option>
         </select>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>시작일</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">시작일</small>
         <b-form-input v-model="postCreateDate.start_date" type="date"></b-form-input>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>종료일</label>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">종료일</small>
         <b-form-input v-model="postCreateDate.end_date" type="date"></b-form-input>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>카테고리</label>
-        <b-form-select v-model="postCreateDate.category" :options="options"></b-form-select>
+      <b-col sm="12" md="6" class="px-1">
+        <small class="formtitle ml-3 float-left">카테고리</small>
+        <b-form-select value="f02c" v-model="postCreateDate.category" :options="options"></b-form-select>
       </b-col>
-      <b-col sm="6" class="px-1">
-        <label>태그</label>
+      <b-col sm="12" md="6" class="px-1">
+        <b-input-group size="sm" class="taggroup mb-2">
+          <b-icon icon="tags-fill" variant="warning" class="mx-2 mt-2"></b-icon>
         <!-- <b-form-input v-model="postCreateDate.tag" placeholder="태그" type="text"></b-form-input> -->
-        <b-form-tags input-id="tags-basic" v-model="postCreateDate.tag" placeholder="태그" class="mb-2"></b-form-tags>
+          <b-form-tags input-id="tags-basic" 
+        tag-variant="warning" v-model="postCreateDate.tag" placeholder="태그" class="rounded mb-2 p-2"></b-form-tags>
+        </b-input-group>
       </b-col>
       <b-col sm="12" class="px-1">
-        <label>내용</label>
+        <small class="formtitle ml-3 float-left">내용</small>
         <b-form-textarea v-model="postCreateDate.data" placeholder="내용" rows="3"></b-form-textarea>
       </b-col>
-    </b-row>
-    <div v-if="!postCreateDate.background_image">
-      <h2>Select an image</h2>
-      <input type="file" @change="onFileChange" />
-    </div>
-    <div v-else>
-      <img :src="postCreateDate.background_image" />
-      <button @click="removeImage">Remove image</button>
-    </div>
-    <div class="d-flex inline justify-content-center">
+    <b-col>
+    <!-- <div>
+      <div v-if="!postCreateDate.background_image">
+        <small class="formtitle  ml-3 float-left">Background Image</small>
+        <input type="file" @change="onFileChange" />
+      </div>
+      <div v-else>
+        <img :src="postCreateDate.background_image" class="w-25"/>
+        <button @click="removeImage">Remove image</button>
+      </div>
+    </div> -->
+    <div class="d-flex inline justify-content-center mt-2 mb-5">
       <div class="p-3">
-        <div @click="postCreate(postCreateDate.tmp=1)" class="btn btn-warning btn-sm">SUBMIT</div>
+        <div @click="postCreate(postCreateDate.tmp=1)" class="btn btn-outline-warning btn-sm">SUBMIT</div>
       </div>
     </div>
+    </b-col>
+    </b-row>
   </div>
   </div>
   </div>
@@ -84,6 +95,7 @@ export default {
   name: "PostCreate",
   data: () => {
     return {
+      images: require('../../assets/img/logo.png'),
       profileInfo: [],
       options: [
         { value: null, text: '카테고리' },
@@ -211,7 +223,6 @@ export default {
 </script>
 
 <style>
-
 label{
   position: relative;
   right: 40%;
@@ -228,9 +239,25 @@ label{
     font-size: 18px;
     cursor: pointer;
 }
-
+/* .b-form-tags{
+  transform : translateY(20px)
+} */
 .formtitle{
-  transform:translateY(10px);
+  padding: 0 4px;
+  transform: translateY(5px);
   background-color:white;
+  font-weight: bolder;
+}
+.formtitle1{
+  padding: 0 4px;
+  transform:translateX(30px) translateY(5px);
+  background-color:white;
+  font-weight: bolder;
+}
+.logo{
+  width: 7vw;
+}
+.taggroup{
+  margin-top: 20px;
 }
 </style>
--- frontend/src/page/user/memberProfile.vue
+++ frontend/src/page/user/memberProfile.vue
@@ -1,95 +1,144 @@
 <template>
   <div class="container">
-    <h2>Member Profile</h2>
-    <div class="row row-cols-2 m-5">
-      <div class="mb-5 py-5 col-12 col-md-3 card">
-        <div v-if="!profileInfo.profile_image">
-          <img class="border rounded-circle" src="../../assets/img/defualt_image.png" />
+    <div class="row d-flex justify-content-center">
+      <div class="center">
+        <div class="profile">
+          <div class="image">
+            <div class="circle-1"></div>
+            <div class="circle-2"></div>
+
+            <div v-if="!profileInfo.profile_image">
+              <img src="../../assets/img/defualt_image.png" width="70" height="70"/>
+            </div>
+            <div v-else>
+              <img :src="profileInfo.profile_image" width="70" height="70"/>
+            </div>
+
+          </div>
+          
+          <div class="name">{{ profileInfo.nickname }}</div>
+          <div class="job">{{ profileInfo.email }}</div>
+          
+          <!-- <div class="actions">
+            <b-button class="btn" v-b-modal.modal-1>수정</b-button>
+            <b-modal @ok="userUpdate" id="modal-1" title="회원정보 수정">
+              <small>닉네임</small>
+              <b-form-input v-model="updateData.nickname"></b-form-input>
+              <small>이메일</small>
+              <b-form-input v-model="updateData.email"></b-form-input>
+              <small>비밀번호</small>
+              <b-form-input type="password" v-model="updateData.password"></b-form-input>
+              <small>자기소개</small>
+              <b-form-textarea v-model="updateData.intro" rows="3" max-rows="6"></b-form-textarea>
+              <small>프로필</small>
+              <div v-if="!updateData.profile_image">
+                <h2>Select an image</h2>
+                <input type="file" @change="onChangeImages">
+              </div>
+              <div v-else>
+                <img :src="updateData.profile_image" style="width: 100%;"/>
+                <button @click="removeImage">Remove image</button>
+              </div>
+            </b-modal>
+
+            <b-button class="btn" v-b-modal.modal-2>탈퇴</b-button>
+            <b-modal @ok="userDelete" id="modal-2" title="회원 탈퇴">
+              <div>
+                <small>이메일</small>
+                <b-form-input v-model="deleteData.email" placeholder="가입 시 작성한 이메일을 입력해주세요"></b-form-input>
+              </div>
+              <div>
+                <small>비밀번호</small>
+                <b-form-input type="password" v-model="deleteData.password" placeholder="가입 시 작성한 비밀번호를 입력해주세요"></b-form-input>
+              </div>
+            </b-modal>
+          </div> -->
         </div>
-        <div v-else>
-          <img class="border rounded-circle" :src="profileInfo.profile_image" />
-        </div>
-        <br />
-        <div class="px-2">
-          <p>{{ profileInfo.nickname }}님</p>
-          <!-- <p>email : {{ email }}</p> -->
-          <!-- <small>소개 : {{ profileInfo.intro }}</small> -->
-        </div>
-        <div class="mt-3">
-          <b-badge @click="goUpdate" class="clickbtn btn-sm mr-2" variant="info">수정</b-badge>
-          <b-badge @click="goDelete" class="clickbtn btn-sm" variant="danger">탈퇴</b-badge>
-        </div>
-        <hr />
-        <div class>
-          <p class="pt-3 px-3 text-info">마일리지 log</p>
-          <small class="px-3">10점</small>
-          <small class="px-3">10점</small>
-          <small class="px-3">10점</small>
+        
+        <div class="stats">
+          <div class="box">
+            <span class="value">5</span>
+            <span class="parameter">가입중인 스터디</span>
+          </div>
+          <div class="box">
+            <span class="value">1387</span>
+            <span class="parameter">마일리지</span>
+          </div>
+          <div class="box">
+            <span class="value">4.6</span>
+            <span class="parameter">랭크</span>
+          </div>
         </div>
       </div>
-      <div class="text-left pt-3 col-12 col-md-8">
-          <h4>평가</h4>
-        <!-- <div class="pt-3 mb-5">
-          <p class="text-info">내가 만든 스터디 list</p>
-          <div v-for="studylist of studyLists" :key="studylist.id">
-            <div
-              v-if="studylist.isleader&&studylist.empId.study.tmp==1"
-              class="tmplist card my-2 px-3 p-2 w-100"
-            >
-              <div @click="goStudy(studylist.pid)" class="d-flex inline">
-                <small>{{ studylist.empId.study.title }}</small>
-                <b-badge class="ml-auto" variant="danger">팀장</b-badge>
-              </div>
-            </div>
-            <div v-if="!studylist.isleader" class="tmplist card my-2 px-3 p-2 w-100">
-              <div @click="goStudyMain(studylist.pid)" class="d-flex inline">
-                <small>{{ studylist.empId.study.title }}</small>
-                <b-badge class="ml-auto" variant="success">진행중</b-badge>
-              </div>
-            </div>
-          </div>
-          <div v-for="studylist of readyLists" :key="studylist.id">
-            <div v-if="!studylist.isleader" class="tmplist card my-2 px-3 p-2 w-100">
-              <div @click="goStudy(studylist.pid)" class="d-flex inline">
-                <small>{{ studylist.empId.study.title }}</small>
-                <b-badge class="ml-auto" variant="warning">대기중</b-badge>
-              </div>
-            </div>
-          </div>
-        </div> -->
-
-        <!-- <div class="pt-3 mb-5">
-          <p class="text-info">임시저장 list</p>
-          <div v-for="tmplist of tmplists" :key="tmplist.pid">
-            <div @click="goTmp(tmplist.pid)" class="tmplist card my-2 px-3 p-2 w-100">
+      <div class="side">
+        <div class="text-left pt-3 col-12">
+          <small class="text-success font-weight-bold text-left pl-3">진행중 스터디</small>
+            <div v-for="list in readyLists" :key="list.id" class=" card m-2 px-2 p-2" @click="goStudyMain(list.pid)">
               <div class="d-flex inline">
-                <small v-if="tmplist.title">{{ tmplist.title }}</small>
-                <small v-else>제목 없음</small>
-                <small class="text-muted ml-auto">{{changeDatedata(tmplist.posttime)}}</small>
+                <small class=" text-left">{{ list.empId.study.title }}</small>
+                <b-badge class="ml-auto my-auto" variant="secondary">승인대기중</b-badge>
               </div>
             </div>
-          </div>
-        </div> -->
 
+            <div v-for="list in leaderListsTmp0" :key="list.id" class=" card m-2 px-2 p-2" @click="goStudyMain(list.pid)">
+              <div class="d-flex inline">
+                <small class=" text-left">{{ list.empId.study.title }}</small>
+                <b-badge class="ml-auto my-auto" variant="danger">진행중&팀장</b-badge>
+              </div>
+            </div>
+
+            <div v-for="list in unleaderLists" :key="list.id" class=" card m-2 px-2 p-2" @click="goStudyMain(list.pid)">
+              <div class="d-flex inline">
+                <small class=" text-left">{{ list.empId.study.title }}</small>
+                <b-badge class="ml-auto my-auto" variant="success">신청완료</b-badge>
+              </div>
+            </div>
+            <hr>
+
+            <small class="text-warning font-weight-bold text-left pl-3 pb-2">모집중 스터디</small>
+            <div v-for="list in leaderListsTmp1" :key="list.id" class=" card m-2 px-2 p-2" @click="goStudyMain(list.pid)">
+              <div class="d-flex inline">
+                <small class=" text-left">{{ list.empId.study.title }}</small>
+                <b-badge class="ml-auto my-auto" variant="warning">모집중&팀장</b-badge>
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import "../../assets/css/profile.scss";
 import axios from "axios";
 import constants from "../../lib/constants";
 
 const SERVER_URL = "http://localhost:8080";
 
 export default {
-  name: "memberprofile",
+  name: "profile",
   data: () => {
     return {
-      profileInfo:{},
+      profileInfo: {},
       studyLists: {},
-      readyLists: {},
       tmplists: {},
+      updateData: {
+        email: "",
+        nickname: "",
+        password: "",
+        intro: "",
+        profile_image: "",
+        uid: "",
+      },
+      deleteData: {
+        email: "",
+        password: "",
+      },
+      readyLists:[],
+      leaderLists:[],
+      unleaderLists:[],
+      leaderListsTmp1:[],
+      leaderListsTmp0:[],
     };
   },
   methods: {
@@ -98,11 +147,16 @@ export default {
     },
     addprofileInfo() {
       const token = this.$cookies.get("Auth-Token");
-      axios.get(SERVER_URL + "/account/profile", {params: {Token: token,},})
+      axios.get(SERVER_URL + "/account/profile", {params: {Token: token}})
       .then((res) => {
-        console.log(res.data.object)
         this.profileInfo = res.data.object
-        
+        console.log("pro", this.profileInfo)
+        this.updateData.email = res.data.object.email
+        this.updateData.nickname = res.data.object.nickname
+        this.updateData.password = res.data.object.password
+        this.updateData.uid = res.data.object.uid
+        this.updateData.intro = res.data.object.intro
+        this.updateData.profile_image = res.data.object.profile_image
         this.addTmpList()
         this.addReadyList() 
         this.addStudyList()
@@ -116,15 +170,20 @@ export default {
     },
     addTmpList() {
       axios.post(SERVER_URL + "/account/tmplist", { nickname: this.profileInfo.nickname })
-      .then((res) => {console.log(res.data.object);this.tmplists = res.data.object})
+      .then((res) => {console.log("tmplist", res.data.object);this.tmplists = res.data.object})
       .catch((err) => {console.log(err)})
     },
     addStudyList() {
       axios
         .post(SERVER_URL + "/account/studylist", this.profileInfo)
         .then((res) => {
-          this.studyLists = res.data.object;
-          console.log(this.studyLists)
+          this.leaderLists = res.data.object.filter(item => item.isleader!=0)
+          this.leaderListsTmp0 = this.leaderLists.filter(item => item.empId.study.tmp == 0)
+          this.leaderListsTmp1 = this.leaderLists.filter(item => item.empId.study.tmp == 1)
+
+
+          this.unleaderLists = res.data.object.filter(item => item.isleader==0)
+          console.log("studylist", this.leaderLists)
         })
         .catch((err) => {
           console.log(err);
@@ -135,12 +194,13 @@ export default {
         .post(SERVER_URL + "/account/readylist", this.profileInfo)
         .then((res) => {
           this.readyLists = res.data.object;
-          console.log(res.data.object);
+          console.log("readylist", res.data.object);
         })
         .catch((err) => {
           console.log(err);
         });
     },
+
     goStudy(post_id) {
       this.$router.push({name: constants.URL_TYPE.POST.POSTDETAIL, params: { post_id: post_id },});
     },
@@ -150,11 +210,42 @@ export default {
     goTmp(post_id) {
       this.$router.push({name: constants.URL_TYPE.POST.POSTUPDATE,params: { post_id: post_id },});
     },
-    goUpdate() {
-      this.$router.push({ name: constants.URL_TYPE.USER.UPDATE });
+    onChangeImages(e) {
+      const file = e.target.files[0];
+      this.updateData.profile_image = URL.createObjectURL(file);
     },
-    goDelete() {
-      this.$router.push({ name: constants.URL_TYPE.USER.DELETE });
+    removeImage: function (e) {
+      this.updateData.profile_image = '';
+    },
+    userUpdate() {
+      axios
+        .post(SERVER_URL + "/account/update", this.updateData)
+        .then(res => {
+          this.$cookies.remove("Auth-Token");
+          const token = res.data.object
+          this.$cookies.set('Auth-Token', token);
+          alert("수정되었습니다.");
+          this.$router.go();
+        })
+        .catch(err => {
+          // this.$router.push({name: constants.URL_TYPE.ERROR.ERRORPAGE,params:{'code':err.response.data}});
+          console.log(err.response.data.errors[0]);
+          alert(err.response.data.errors[0].field + "를 확인해 주세요")
+        });
+    },
+    userDelete() {
+      axios
+        .post(SERVER_URL + "/account/delete", this.deleteData)
+        .then(res => {
+          this.$cookies.remove("Auth-Token");
+          alert("회원탈퇴되었습니다.");
+          this.$router.push("/");
+          // this.$router.go();
+        })
+        .catch(err => {
+          alert("입력정보를 확인해주세요.");
+          console.log(err);
+        });
     },
   },
   created() {
