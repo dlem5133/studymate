d2e715308f9dba957cf5f88a431970710025ede2
김성웅 <sksk9877@naver.com>
2020-08-05 14:30:45 +0900

    0805_admin_page

--- frontend/src/page/AdminPage.vue
+++ frontend/src/page/AdminPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
     <h1>ADMIN PAGE</h1>
-    <hr>
+    <hr />
     <div>
       <b-tabs content-class="mt-3">
         <b-tab title="유저">
           <div class="input-group m-1" @keypress.enter="searchUser(findUser)">
-            <input class="form-control" v-model="findUser" type="text" placeholder="닉네임을 입력해주세요">
+            <input class="form-control" v-model="findUser" type="text" placeholder="닉네임을 입력해주세요" />
             <button class="input-group-text" @click="searchUser(findUser)">
               <i class="fas fa-search"></i>
             </button>
@@ -31,7 +31,9 @@
                 <td>{{user.mileage}}</td>
                 <td>{{((user.score1 + user.score2 + user.score3) / 3).toFixed(1)}}</td>
                 <td>{{user.penalty}}</td>
-                <td><i class="fas fa-user-slash" @click="userDelete(user)"></i></td>
+                <td>
+                  <i class="fas fa-user-slash" @click="userDelete(user)"></i>
+                </td>
               </tr>
             </tbody>
             <b-button class="d-flex plusBnt" @click="plusUserPage" variant="link">
@@ -42,7 +44,7 @@
         </b-tab>
         <b-tab @click="allStudy" title="스터디">
           <div class="input-group m-1" @keypress.enter="searchStudy(findStudy)">
-            <input class="form-control" v-model="findStudy" type="text" placeholder="타이들을 입력해주세요">
+            <input class="form-control" v-model="findStudy" type="text" placeholder="타이들을 입력해주세요" />
             <button class="input-group-text" @click="searchStudy(findStudy)">
               <i class="fas fa-search"></i>
             </button>
@@ -65,7 +67,9 @@
                 <td>{{study.data}}</td>
                 <td>{{study.category}}</td>
                 <td>{{study.posttime}}</td>
-                <td><i class="fas fa-trash-alt" @click="studyDelete(study)"></i></td>
+                <td>
+                  <i class="fas fa-trash-alt" @click="studyDelete(study)"></i>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -97,17 +101,23 @@
                 <td>{{reportUser[1].penalty}}</td>
               </tr>
               <b-modal :id="`bv-modal-example_${ri}`" hide-footer>
-                <template v-slot:modal-title>
-                  신고내용
-                </template>
+                <template v-slot:modal-title>신고내용</template>
                 <div class="d-block text-center">
                   <h5 class="pb-3">스터디명 : {{reportUser[2].title}}</h5>
-                  <b-list-group class="m-2 text-left" v-for="content in reportUser[0]" :key="content.no">
+                  <b-list-group
+                    class="m-2 text-left"
+                    v-for="content in reportUser[0]"
+                    :key="content.no"
+                  >
                     <b-list-group-item>{{content.reason}}</b-list-group-item>
                   </b-list-group>
                   <div>
-                    <i class="fas fa-user-slash mr-2" @click="givePenalty(reportUser[0][0])"><small> 페널티 부여</small></i>
-                    <i class="fas fa-undo" @click="noPenalty(reportUser[0][0])"><small> 반려</small></i>
+                    <i class="fas fa-user-slash mr-2" @click="givePenalty(reportUser[0][0])">
+                      <small>페널티 부여</small>
+                    </i>
+                    <i class="fas fa-undo" @click="noPenalty(reportUser[0][0])">
+                      <small>반려</small>
+                    </i>
                   </div>
                 </div>
               </b-modal>
@@ -135,7 +145,9 @@
                 <td>{{banUser.mileage}}</td>
                 <td>{{((banUser.score1 + banUser.score2 + banUser.score3) / 3).toFixed(1)}}</td>
                 <td>{{banUser.penalty}}</td>
-                <td><i class="fas fa-user-slash"></i></td>
+                <td>
+                  <i class="fas fa-user-slash"></i>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -162,96 +174,97 @@ export default {
       findStudy: "",
       userPage: 5,
       studyPage: 5,
-    }
+    };
   },
-  created () {
-    this.allUser()
+  created() {
+    this.allUser();
   },
   methods: {
     allUser() {
-      axios.post(SERVER_URL + "/admin/allUser")
-      .then(res => {
-        const allUsers = res.data.object
-        this.users = res.data.object.filter(allUsers => allUsers.penalty < 3)
-        this.banUsers = res.data.object.filter(allUsers => allUsers.penalty >= 3)
-      })
+      axios.post(SERVER_URL + "/admin/allUser").then((res) => {
+        const allUsers = res.data.object;
+        this.users = res.data.object.filter((allUsers) => allUsers.penalty < 3);
+        this.banUsers = res.data.object.filter(
+          (allUsers) => allUsers.penalty >= 3
+        );
+      });
     },
     userDelete(user) {
       const deleteData = {
         email: user.email,
         password: user.password,
-      }
+      };
       axios
-      .post(SERVER_URL + "/account/delete", deleteData)
-      .then(() => {
-        alert("회원을 삭제했습니다.")
-        this.allUser()
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .post(SERVER_URL + "/account/delete", deleteData)
+        .then(() => {
+          alert("회원을 삭제했습니다.");
+          this.allUser();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     allStudy() {
-      axios.post(SERVER_URL + "/admin/allStudy")
-      .then(res => {
-        this.studies = res.data.object
-      })
+      axios.post(SERVER_URL + "/admin/allStudy").then((res) => {
+        this.studies = res.data.object;
+      });
     },
     studyDelete(study) {
       axios
-      .post(SERVER_URL + "/study/delete", study)
-      .then(() => {
-        alert("스터디를 삭제했습니다.");
-        this.allStudy()
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .post(SERVER_URL + "/study/delete", study)
+        .then(() => {
+          alert("스터디를 삭제했습니다.");
+          this.allStudy();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     allReport() {
-      axios.post(SERVER_URL + "/admin/report")
-      .then(res => {
-        this.reports = res.data.object
-      })
+      axios.post(SERVER_URL + "/admin/report").then((res) => {
+        this.reports = res.data.object;
+      });
     },
     givePenalty(user) {
-      axios.post(SERVER_URL + "/admin/givepenalty", user)
-      .then(() => {
-        this.allReport()
-      })
+      axios.post(SERVER_URL + "/admin/givepenalty", user).then(() => {
+        this.allReport();
+      });
     },
     noPenalty(user) {
-      axios.post(SERVER_URL + "/admin/nopenalty", user)
-      .then(() => {
-        this.allReport()
-      })
+      axios.post(SERVER_URL + "/admin/nopenalty", user).then(() => {
+        this.allReport();
+      });
     },
     searchUser(tmp) {
-      this.allUser()
+      this.allUser();
       setTimeout(() => {
-        const newUsers = this.users.filter(user => user.nickname.includes(tmp))
-        this.users = newUsers
+        const newUsers = this.users.filter((user) =>
+          user.nickname.includes(tmp)
+        );
+        this.users = newUsers;
       }, 100);
     },
     searchStudy(tmp) {
-      this.allStudy()
+      this.allStudy();
       setTimeout(() => {
-        const newStudies = this.studies.filter(study => study.title.includes(tmp))
-        this.studies = newStudies
+        const newStudies = this.studies.filter((study) =>
+          study.title.includes(tmp)
+        );
+        this.studies = newStudies;
       }, 100);
     },
     plusUserPage() {
-      this.userPage += 5
+      this.userPage += 5;
     },
     plusStudyPage() {
-      this.studyPage += 5
+      this.studyPage += 5;
     },
   },
-}
+};
 </script>
 
 <style scope>
-tbody :hover{
+tbody :hover {
   background-color: #eee;
 }
 i {
