21faa6656911eb7ecc3c86008ffcdd6867bcbe69
김성웅 <sksk9877@naver.com>
2020-08-17 15:46:35 +0900

    error_page

--- frontend/src/page/error/ErrorPage.vue
+++ frontend/src/page/error/ErrorPage.vue
@@ -6,6 +6,8 @@
       {{code.status}}
       {{code.error}}
       <br />
+      <p>로그인 시간이 초과되어 재 로그인이 필요합니다.</p>
+      <p>5 초 뒤 메인페이지로 이동합니다.</p>
       <a href="/" class="text-info">홈페이지</a>로 이동하여 다시 시도해보세요.
     </div>
   </div>
@@ -20,12 +22,18 @@ export default {
     };
   },
   created() {
-    // this.tokenDelete()
+    this.tokenDelete()
   },
   methods: {
-    // tokenDelete() {
-    //   this.$cookies.remove("Auth-Token");
-    // }
+    tokenDelete() {
+      this.$cookies.remove("Auth-Token");
+      this.decreasetime()
+    },
+    decreasetime() {
+      setTimeout(() => {
+        this.$router.push("/")
+      }, 5000);
+    }
   },
 };
 </script>
